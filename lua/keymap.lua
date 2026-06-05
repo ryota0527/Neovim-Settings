60c50e6d180a9b21b051011574cc03c2a18c3400
ryota0527 <yamazaki.ryota.s3@dc.tohoku.ac.jp>
2026-06-05 23:14:16 +0900

    gl and gh keymappings added in visual mode

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -11,6 +11,8 @@ vim.keymap.set("v", "<A-k>", "10k", { noremap = true })
 vim.keymap.set("v", "<A-l>", "10l", { noremap = true })
 vim.keymap.set("v", "<Tab>", ">gv", { noremap = true })
 vim.keymap.set("v", "<S-Tab>", "<gv", { noremap = true })
+vim.keymap.set("v", "gh", "^", { noremap = true })
+vim.keymap.set("v", "gl", "$", { noremap = true })
 
 -- normal mode
 vim.keymap.set("n", "<A-h>", "10h", { noremap = true })
