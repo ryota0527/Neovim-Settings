adbdd7eaa26c7d52e7978e0fc2ba2f15938163c7
ryota0527 <yamazaki.ryota.s3@dc.tohoku.ac.jp>
2026-09-12 13:42:38 +0900

    unnecessary comments deleted

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -68,8 +68,5 @@ return {
         })
     end,
 
-    -- Optional dependencies
     dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
 }
