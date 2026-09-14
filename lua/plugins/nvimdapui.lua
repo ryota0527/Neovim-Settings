6e4a1f3a575109378d33ed536425e17f438ae1bd
ryota0527 <yamazaki.ryota.s3@dc.tohoku.ac.jp>
2026-09-15 01:24:41 +0900

    caught up with master

--- lua/plugins/nvimdapui.lua
+++ lua/plugins/nvimdapui.lua
@@ -25,32 +25,10 @@ return {
                     position = "bottom",
                 },
             },
-
-            keys = {
-                {
-                    "<leader>du",
-                    function()
-                        dapui.toggle()
-                    end,
-                },
-                {
-                    "<leader>de",
-                    function()
-                        dapui.eval()
-                    end,
-                },
-            },
         })
 
         dap.listeners.after.event_initialized.dapui_config = function()
             dapui.open()
         end
-        dap.listeners.before.event_terminated.dapui_config = function()
-            dapui.close()
-        end
-        dap.listeners.before.event_exited.dapui_config = function()
-            dapui.close()
-        end
-
     end,
 }
