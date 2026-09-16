61a6ce22cdf1c080be66c139b74db04d312cdfac
ryota0527 <yamazaki.ryota.s3@dc.tohoku.ac.jp>
2026-09-17 00:40:00 +0900

    fixed the width of terminal window

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -2,4 +2,8 @@
 vim.api.nvim_create_user_command("T", function()
     vim.cmd("botright vsplit")
     vim.cmd("terminal")
+    vim.api.nvim_win_set_width(
+        vim.api.nvim_get_current_win(),
+        55
+    )
 end, {})
